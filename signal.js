1dbd24e580271ca74ac010e4a0b9953edd1d56d3
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-21 21:19:40 +0200

    add signal.js

---
+++ signal.js
@@ -0,0 +1,40 @@
+/**
+ * (c) 2016 Ruben Schmidmeister
+ */
+
+/**
+ *
+ * @template T
+ * @constructor
+ */
+export function Signal () {
+
+}
+
+/**
+ *
+ * @param {(function(value: T))} callbackFn
+ */
+Signal.prototype.addListener = function (callbackFn) {
+  if (this._listeners == null) {
+    this._listeners = new Set()
+  }
+
+  this._listeners.add(callbackFn)
+}
+
+Signal.prototype.removeListener = function (callbackFn) {
+  if (this._listeners == null) {
+    return
+  }
+
+  this._listeners.remove(callbackFn)
+}
+
+/**
+ *
+ * @param {T} [value]
+ */
+Signal.prototype.dispatch = function (value) {
+  this._listeners.forEach((listener) => listener(value))
+}
